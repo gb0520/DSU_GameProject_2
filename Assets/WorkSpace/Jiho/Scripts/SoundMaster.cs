8ce4b18795153d2b069acfc94ece0b6fc3735860
jjjiiihhhooo <thdwlgh950@naver.com>
2023-04-20 15:04:16 +0900

    소리 후방주의

--- Assets/WorkSpace/Jiho/Scripts/SoundMaster.cs
+++ Assets/WorkSpace/Jiho/Scripts/SoundMaster.cs
@@ -2,59 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum Sound
-{
-    Bgm, Effect, 
-    MaxCount, //그냥 사운드 갯수 세는 용도 별 의미없음
-}
 
 public class SoundMaster : MonoBehaviour
 {
-    AudioSource[] audioSources = new AudioSource[(int)Sound.MaxCount];
+    public AudioClip[] clips;
+    [SerializeField] private AudioSource[] audioSources;
 
-    public void SoundInit()
-    {
-        string[] soundNames = System.Enum.GetNames(typeof(Sound)); // "Bgm", "Effect"
-
-        for(int i = 0; i < soundNames.Length - 1; i++)
-        {
-            GameObject temp = new GameObject { name = soundNames[i] };
-            audioSources[i] = temp.AddComponent<AudioSource>();
-            temp.transform.parent = this.transform;
-        }
-
-        audioSources[(int)Sound.Bgm].loop = true; //얘는 무한 루프
-    }
-
-    public void SoundClear()
-    {
-        foreach(AudioSource audioSource in audioSources)
-        {
-            audioSource.clip = null;
-            audioSource.Stop();
-        }
-    }
-
-    public void Play(AudioClip audioClip, Sound type = Sound.Effect, float pitch = 1.0f)
+    public void Play(AudioClip audioClip, bool _isBgm, float pitch = 1.0f)
     {
         if (audioClip == null)
             return;
 
-        if (type == Sound.Bgm) // BGM 배경음악 재생
+        if (_isBgm) // BGM 배경음악 재생
         {
-            AudioSource audioSource = audioSources[(int)Sound.Bgm];
-            if (audioSource.isPlaying)
-                audioSource.Stop();
+           
+            if (audioSources[0].isPlaying)
+                audioSources[0].Stop();
 
-            audioSource.pitch = pitch;
-            audioSource.clip = audioClip;
-            audioSource.Play();
+            audioSources[0].pitch = pitch;
+            audioSources[0].clip = audioClip;
+            audioSources[0].Play();
         }
         else // Effect 효과음 재생
         {
-            AudioSource audioSource = audioSources[(int)Sound.Effect];
-            audioSource.pitch = pitch;
-            audioSource.PlayOneShot(audioClip);
+            audioSources[1].pitch = pitch;
+            audioSources[1].PlayOneShot(audioClip);
         }
     }
 
